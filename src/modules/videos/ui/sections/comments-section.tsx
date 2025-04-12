26c71711061ba3f1540c6acbe8117550682b10c5
Mustafa Lanewala <125785421+mustafalanewala@users.noreply.github.com>
2025-04-12 22:52:08 +0530

    Implement comment deletion and infinite scrolling for comments; add remove mutation, update comments fetching logic, and enhance UI components for better user interaction

--- src/modules/videos/ui/sections/comments-section.tsx
+++ src/modules/videos/ui/sections/comments-section.tsx
@@ -1,8 +1,11 @@
 "use client"
 
+import { InfiniteScroll } from "@/components/infinite-scroll"
+import { DEFAULT_LIMIT } from "@/constants"
 import { CommentForm } from "@/modules/comments/ui/components/comment-form"
 import { CommentItem } from "@/modules/comments/ui/components/comment-item"
 import { trpc } from "@/trpc/client"
+import { Loader2Icon } from "lucide-react"
 import { Suspense } from "react"
 import { ErrorBoundary } from "react-error-boundary"
 
@@ -22,26 +25,36 @@ export const CommentsSection = ({ videoId }: CommentsSectionProps) => {
 
 export const CommentsSectionSkeleton = () => {
   return (
-    <h1></h1>
+    <div className="mt-6 flex justify-center items-center">
+      <Loader2Icon className="text-muted-foreground size-7 animate-spin" />
+    </div>
   )
 }
 
 const CommentsSectionSuspense = ({ videoId }: CommentsSectionProps) => {
-  const [comments] = trpc.comments.getMany.useSuspenseQuery({ videoId })
+  const [comments, query] = trpc.comments.getMany.useSuspenseInfiniteQuery({ videoId, limit: DEFAULT_LIMIT }, {
+    getNextPageParam: (lastPage) => lastPage.nextCursor,
+  })
   return (
     <div className="mt-6">
       <div className="flex flex-col gap-6">
-        <h1>
-          0 Comments
+        <h1 className="text-xl font-bold">
+          {comments.pages[0].totalCount} Comments
         </h1>
         <CommentForm videoId={videoId} />
         <div className="flex flex-col gap-4 mt-2">
-          {comments.map((comment) => (
+          {comments.pages.flatMap((page) => page.items).map((comment) => (
             <CommentItem
               key={comment.id}
               comment={comment}
             />
           ))}
+          <InfiniteScroll
+            isManual
+            hasNextPage={query.hasNextPage}
+            isFerchingNextPage={query.isFetchingNextPage}
+            fetchNextPage={query.fetchNextPage}
+          />
         </div>
       </div>
     </div>
